29856b1a13e35b4fbc9782bc3dd654ff62b05c4b
Omidp <omidpourhadi@gmail.com>
2017-02-14 18:03:30 +0330

    add jbpm

--- src/main/java/org/omidbiz/seamcat/CustomerHome.java
+++ src/main/java/org/omidbiz/seamcat/CustomerHome.java
@@ -2,12 +2,26 @@ package org.omidbiz.seamcat;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Out;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Transactional;
+import org.jboss.seam.annotations.bpm.CreateProcess;
+import org.jboss.seam.bpm.ManagedJbpmContext;
 import org.jboss.seam.framework.EntityHome;
+import org.jbpm.graph.exe.ProcessInstance;
 
 @Name("customerHome")
 @Scope(ScopeType.CONVERSATION)
 public class CustomerHome extends EntityHome<Customer>
 {
 
+	
+	@Transactional	
+	public void register()
+	{
+		//Since there is no EJB interception on tomcat you can't use @CreateProcess annotation on method
+		ProcessInstance process = ManagedJbpmContext.instance().newProcessInstanceForUpdate("managerProcess");
+		process.getContextInstance().setVariable("responsible", "admin");
+	}
+	
 }
